08380f558f1802d4e86f82d3be8a4298a22f9363
Rero0124 <kimseung0930@gmail.com>
2025-05-03 07:24:00 +0900

    fix: 친구의 프로필 대신 자신의 프로필을 가져오던 오류 수정

--- app/api/friends/route.ts
+++ app/api/friends/route.ts
@@ -29,7 +29,7 @@ export async function GET(request: NextRequest) {
 				isOnline: true,
 			},
 			where: {
-				friendByProfile: {
+				friendProfile: {
 					some: {
 						byProfile: {
 							id: sessionCheck.profileId,
